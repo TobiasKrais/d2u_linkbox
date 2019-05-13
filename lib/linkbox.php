767c556966ff3cac36715a5a913374f199c4b6b6
Tobias Krais <tk@design-to-use.de>
2019-05-13 17:56:32 +0200

    add additional link types

--- lib/linkbox.php
+++ lib/linkbox.php
@@ -272,9 +272,6 @@ class Linkbox implements \D2U_Helper\ITranslationHelper {
 		if($this->link_type == "document" && $this->document != "") {
 			$this->link = rex_url::media($this->document);
 		}
-		else if($this->link_type == "article" && $this->article_id > 0) {
-			$this->link = rex_getUrl($this->article_id);
-		}
 		else if($this->link_type == "url" && $this->external_url != "") {
 			$this->link = $this->external_url;
 		}
@@ -295,6 +292,9 @@ class Linkbox implements \D2U_Helper\ITranslationHelper {
 				$used_machine = new \UsedMachine($this->link_addon_id, $this->clang_id);
 				$this->link = $used_machine->getURL();
 			}
+		}
+		else if(($this->link_type == "" || $this->link_type == "article") && $this->article_id > 0) {
+			$this->link = rex_getUrl($this->article_id);
 		}
 		
 		return $this->link;
