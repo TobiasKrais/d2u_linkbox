0f3e810c341ef0da874cb412eb7079076c1782aa
Tobias Krais <tk@design-to-use.de>
2026-08-09 11:04:53 +0200

    add changelog note

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -1,6 +1,7 @@
 <h2>Changelog</h2>
 <p>1.6.4:</p>
 <ul>
+	<li>Wartung: Interne CI-Dateien im Verzeichnis <code>.github</code> (GitHub Actions, Dependabot) werden nicht mehr mit dem Installer-Paket ausgeliefert.</li>
 	<li>Bugfix: Der Online/Offline-Umschalter sowie der Löschen-Link in der Linkbox-Liste führten zu einem CSRF-Fehler, weil die Links kein CSRF-Token enthielten. Beide Links hängen das Token jetzt an (analog zu den Prioritäts-Buttons).</li>
 	<li>Bugfix: Modul 24-2 (BS4) gab eine im Überschrift-Feld hinterlegte einfache Formatierung (z. B. <code>&lt;br&gt;</code>) im Frontend als sichtbaren HTML-Code statt gerendert aus. Die Überschrift wird jetzt wie der Teaser als vom Backend-Redakteur gepflegter HTML-Inhalt ausgegeben. Modul-Revision erhöht.</li>
 </ul>
